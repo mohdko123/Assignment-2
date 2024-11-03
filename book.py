bcaf1470ae2e1a1e179bef2313f9abc97ffc4e58
mohdko123 <mohdko123@hotmail.com>
2024-11-03 20:10:33 +0500

    Add files via upload

---
+++ book.py
@@ -0,0 +1,38 @@
+
+class Book:
+    def __init__(self, title, author, publication_date, genre, price):
+        self.__title = title
+        self.__author = author
+        self.__publication_date = publication_date
+        self.__genre = genre
+        self.__price = price
+
+    def get_title(self):
+        return self.__title
+
+    def set_title(self, title):
+        self.__title = title
+
+    def get_author(self):
+        return self.__author
+
+    def set_author(self, author):
+        self.__author = author
+
+    def get_publication_date(self):
+        return self.__publication_date
+
+    def set_publication_date(self, publication_date):
+        self.__publication_date = publication_date
+
+    def get_genre(self):
+        return self.__genre
+
+    def set_genre(self, genre):
+        self.__genre = genre
+
+    def get_price(self):
+        return self.__price
+
+    def set_price(self, price):
+        self.__price = price
